f18e7842c5e73af1ab465f58d48e163c037753f9
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2025-04-04 18:25:05 +0300

    0.15 accordion margins

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                    <div class="accordion" id="accordion-1" style="max-width: 30rem; margin: 1rem auto 2rem;">
+                    <div class="accordion" id="accordion-1" style="margin: 1rem auto 2rem;">
                         @if(!$accordion_texts->isEmpty())
                             @foreach ($accordion_texts as $accordion_text)
                                 <div class="accordion__item">
