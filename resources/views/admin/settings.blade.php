488e009dbf98be1aa1663ffc33d6b933fbff38e8
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2025-04-04 11:12:38 +0300

    0.10 localization

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -6,8 +6,8 @@
 @section('content')
 
 <section class="header-operation container-fluid animated fadeIn d-flex mb-2 align-items-baseline d-print-none" bp-section="page-header">
-    <h1 class="text-capitalize mb-0" bp-section="page-heading">Settings</h1>
-    <p class="ms-2 ml-2 mb-0" bp-section="page-subheading">Page for Settings</p>
+    <h1 class="text-capitalize mb-0" bp-section="page-heading">{{__('Settings')}}</h1>
+    <p class="ms-2 ml-2 mb-0" bp-section="page-subheading">{{__('Page for') . ' ' . __('Settings')}} </p>
 </section>
 
 <section class="content container-fluid animated fadeIn" bp-section="content">
--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,7 +1,7 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<x-backpack::menu-item title="Settings" icon="la la-question" :link="backpack_url('settings')" />
-<x-backpack::menu-item title="Accordion texts" icon="la la-question" :link="backpack_url('accordion-text')" />
+<x-backpack::menu-item title="{{__('Settings')}}" icon="la la-question" :link="backpack_url('settings')" />
+<x-backpack::menu-item title="{{__('Accordion texts')}}" icon="la la-question" :link="backpack_url('accordion-text')" />
 
-<x-backpack::menu-item title="Users" icon="la la-question" :link="backpack_url('user')" />
+<x-backpack::menu-item title="{{__('Users')}}" icon="la la-question" :link="backpack_url('user')" />
